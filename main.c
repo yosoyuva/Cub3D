67fbdd066e3d9f778620ac2243fcfa21e73d1ecc
user42 <mehdiyuva@outlook.fr>
2021-01-05 11:34:37 +0100

    test on minilibx

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main(void)
   void *mlx_ptr;
   void *mlx_win;
 
-  mlx_ptr = mlx_init();
-  mlx_win = mlx_new_window(mlx_ptr, 500, 500, "Hello world!");
-  mlx_loop(mlx_ptr);
+  mlx_ptr = mlx_init();// Connecting to the minilibx and save the ID connection to mlx_ptr
+  mlx_win = mlx_new_window(mlx_ptr, 500, 500, "Hello world!");// Creating a new window and saving the ID of the window to mlx_window
+  mlx_loop(mlx_ptr);// A loop to keep the connection up
 }
